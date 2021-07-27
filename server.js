9952d87b25c2fde7b1c1f64ddeb0510022d445a7
scottskinn <84105761+scottskinn@users.noreply.github.com>
2021-07-27 12:04:50 -0400

    user input

--- server.js
+++ server.js
@@ -43,6 +43,15 @@ function filterByQuery(query, animalsArray) {
     return filteredResults;
 }
 
+function findById(id, animalsArray) {
+    const result = animalsArray.filter(animal => animal.id === id)[0];
+    if (result) {
+        res.json(result);
+    } else {
+        res.send(404);
+    }
+};
+
 app.get('/api/animals', (req, res) => {
     let results = animals;
     console.log(req.query);
@@ -50,6 +59,11 @@ app.get('/api/animals', (req, res) => {
         results = filterByQuery(req.query, results);
     }
     res.json(results);
+});
+
+app.get('/api/animals/:id', (req, res) => {
+    const result = findById(req.params.id, animals);
+        res.json(result);
 });
 
 app.listen(PORT, () => {
