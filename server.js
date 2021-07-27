ac14377aabeeb9dc96bc06528f653cdc646d70a7
scottskinn <84105761+scottskinn@users.noreply.github.com>
2021-07-27 14:23:39 -0400

    add validation

--- server.js
+++ server.js
@@ -73,6 +73,22 @@ function createNewAnimal(body, animalsArray) {
     return animal;
 }
 
+function validateAnimal(animal) {
+    if (!animal.name || typeof animal.name !== 'string') {
+      return false;
+    }
+    if (!animal.species || typeof animal.species !== 'string') {
+      return false;
+    }
+    if (!animal.diet || typeof animal.diet !== 'string') {
+      return false;
+    }
+    if (!animal.personalityTraits || !Array.isArray(animal.personalityTraits)) {
+      return false;
+    }
+    return true;
+  }
+
 app.get('/api/animals', (req, res) => {
     let results = animals;
     console.log(req.query);
@@ -92,11 +108,16 @@ app.post('/api/animals', (req, res) => {
     // set id based on what the nest index of the array will be
     req.body.id = animals.length.toString();
 
+      // if any data in req.body is incorrect, send 400 error back
+    if (!validateAnimal(req.body)) {
+    res.status(400).send('The animal is not properly formatted.');
+    } else {
      // add animal to json file and animals array in this function
     const animal = createNewAnimal(req.body, animals);
 
     console.log(req.body);
     res.json(req.body);
+    }
 });
 
 app.listen(PORT, () => {
